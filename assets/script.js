61330a9df9204e1a0f85aa94279d6b8f88aca519
Leolira1808 <97995984+Leolira1808@users.noreply.github.com>
2022-12-21 15:56:27 -0300

    teste

--- assets/script.js
+++ assets/script.js
@@ -49,3 +49,12 @@ document
 document
   .querySelector("#valorEntrada")
   .addEventListener("keyup", function (event) {});
+
+function MascaraMoeda(i) {
+  var v = i.value.replace(/\D/g, "");
+  v = (v / 100).toFixed(2) + "";
+  v = v.replace(".", ",");
+  v = v.replace(/(\d)(\d{3})(\d{3}),/g, "$1.$2.$3,");
+  v = v.replace(/(\d)(\d{3}),/g, "$1.$2,");
+  i.value = v;
+}
